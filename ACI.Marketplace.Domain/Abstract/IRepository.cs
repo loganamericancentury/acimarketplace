8f5c63c76b35ff1f1ed1d621ea0d9c3888d21754
Logan Manning <Logan_Manning@AmericanCentury.com>
2018-05-22 13:13:54 -0500

    add IRepository

---
+++ ACI.Marketplace.Domain/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACI.Marketplace.Domain.Abstract
+{
+    public interface IRepository
+    {
+        T Insert<T>(T obj) where T : class;
+        T Update<T>(T obj) where T : class;
+    }
+}
